54600c1dc9d95d6a3c3aba2d670b964cffa3cab5
Paul Okello <paulokello273@gmail.com>
2021-08-25 19:16:19 +0300

    ⚡ General Update:  methods on models

--- udemy_backend/courses/models.py
+++ udemy_backend/courses/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 import uuid
+from decimal import Decimal
+from .helpers import get_timer
+from mutagen.mp4 import MP4, MP4StreamInfoError
 
 
 # Sector model
@@ -10,10 +14,14 @@ class Sector(models.Model):
                                    editable=False,
                                    unique=True)
     related_courses = models.ManyToManyField('Course',
-                                             related_name='related_courses')
+                                             related_name='related_courses',
+                                             blank=True)
     sector_image = models.ImageField(upload_to='sector_image',
                                      blank=True,
                                      null=True)
+
+    def get_image_absolute_url(self):
+        return "http://localhost:8000" + self.sector_image.url
 
 
 # Create your models here.
@@ -26,7 +34,7 @@ class Course(models.Model):
                                on_delete=models.CASCADE,
                                related_name="author")
     language = models.CharField(max_length=50)
-    course_section = models.ManyToManyField('CourseSection')
+    course_section = models.ManyToManyField('CourseSection', blank=True)
     comments = models.ManyToManyField('Comment', blank=True)
     image_url = models.ImageField(upload_to='course_images', blank=True)
     course_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
@@ -35,12 +43,42 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
+    def get_brief_description(self):
+        return self.description[:100]
+
+    def get_enrolled_students(self):
+        students = get_user_model().objects.filter(paid_courses=self)
+        return len(students)
+
+    def get_total_classes(self):
+        classes = 0
+        for section in self.course_section:
+            classes += len(section.episodes.all())
+        return classes
+
+    def total_course_lengths(self):
+        lengths = Decimal(0.0)
+        for section in self.course_section:
+            for episode in section.episodes.all():
+                lengths += episode.length
+
+        return get_timer(lengths, type="short")
+
+    def get_image_absolute_url(self):
+        return "http://localhost:8000" + self.image_url.url
+
 
 # CourseSection models
 class CourseSection(models.Model):
     section_title = models.CharField(max_length=255)
-    episodes = models.ManyToManyField('Episode')
+    episodes = models.ManyToManyField('Episode', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def total_length(self):
+        total = Decimal(0.0)
+        for episode in self.episodes.all():
+            total += episode.length
+        return get_timer(total, type="min")
 
 
 # Episode models
@@ -48,6 +86,23 @@ class Episode(models.Model):
     title = models.CharField(max_length=255)
     file = models.FileField(upload_to='course_episodes')
     length = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def get_video_length(self):
+        try:
+            video = MP4(self.file)
+            return video.info.length
+        except MP4StreamInfoError:
+            return 0.0
+
+    def get_video_length_time(self):
+        return get_timer(self.length)
+
+    def get_absolute_url(self):
+        return "http://localhost:8000" + self.file.url
+
+    def save(self, *args, **kwargs):
+        self.length = self.get_video_length()
+        return super().save(*args, **kwargs)
 
 
 # Comment models
